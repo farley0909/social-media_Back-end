93a2b7cdf9e9707ad5ee5426b7bb4d49d8deff9a
José Farley Sousa da Silva <jose.farley@academico.ifpb.edu.br>
2023-02-04 19:19:28 -0300

    Adding the basic structure of the backend

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import {Request, Response} from 'express'
+
+const router = Router()
+
+router.get('/', (request:Request, response:Response) => {
+    response.send("First route")
+})
+
+export {router}
